2fecb9c472b622bab1df7b5e3a7835d8a4dfbaef
Guany <1084072690@qq.com>
2023-10-25 17:43:14 +0800

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { createSSRApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 
 export function createApp() {
   const app = createSSRApp(App)
+  const pinia = createPinia()
+
+  pinia.use(createPersistedState({
+    storage: {
+      getItem: key => uni.getStorageSync(key),
+      setItem: (key, value) => uni.setStorageSync(key, value),
+    },
+  }))
+
+  app.use(pinia)
+
   return {
     app,
   }
