ec9a3bf1444cb2a0413f7226b5dd76378471ef94
Guany <1084072690@qq.com>
2023-10-25 18:21:22 +0800

    feat: add unplugin-auto-import

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 const title = ref('Hello')
 </script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
+  plugins: [
+    AutoImport({
+      imports: [
+        'vue',
+        'uni-app',
+      ],
+      dirs: [
+        './src/composables',
+        './src/stores',
+      ],
+      vueTemplate: true,
+    }),
+    uni(),
+  ],
 })
