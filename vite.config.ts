b24219bff2cd3c241e1ee46b82df528d6aa2eb78
Guany <1084072690@qq.com>
2023-11-12 10:15:21 +0800

    chore: Update paths and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import uni from '@dcloudio/vite-plugin-uni'
@@ -18,4 +19,9 @@ export default defineConfig({
     }),
     uni(),
   ],
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
 })
