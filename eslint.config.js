cf3a45ce4a0bb73c62ed988cbde3581f0c55fdfa
Guany <1084072690@qq.com>
2024-02-19 10:10:13 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
+// @ts-check
 const antfu = require('@antfu/eslint-config').default
 
-module.exports = antfu({
-  rules: {
-    'no-console': 'off',
+module.exports = antfu(
+  {
+    formatters: true,
   },
-})
+  {
+    rules: {
+      'no-console': 'off',
+    },
+  },
+)
